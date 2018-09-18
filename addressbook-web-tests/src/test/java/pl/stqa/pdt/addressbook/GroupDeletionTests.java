36b0aa7708850c6b9a29737e5f6ccda6f3678d6a
Asia Duda <asiaduda@gmail.com>
2018-09-19 00:54:12 +0200

    Created TestBase class - a general base class for tests

---
+++ addressbook-web-tests/src/test/java/pl/stqa/pdt/addressbook/GroupDeletionTests.java
@@ -0,0 +1,15 @@
+package pl.stqa.pdt.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase{
+
+    @Test
+    public void testGroupDeletion() {
+        gotoGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        returnToGroupPage();
+    }
+
+}
