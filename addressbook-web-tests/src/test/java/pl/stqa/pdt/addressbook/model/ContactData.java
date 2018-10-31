f72972a96f55a473154c6d3a2dd7481d352f5460
Asia Duda <asiaduda@gmail.com>
2018-10-31 22:06:48 +0100

    Find the maximum value - changed ContactCreationTests class

--- addressbook-web-tests/src/test/java/pl/stqa/pdt/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pdt/addressbook/model/ContactData.java
@@ -3,7 +3,7 @@ package pl.stqa.pdt.addressbook.model;
 import java.util.Objects;
 
 public class ContactData {
-  private final String id;
+  private int id;
   private final String firstname;
   private final String lastname;
   private final String address;
@@ -12,7 +12,7 @@ public class ContactData {
   private String group;
 
   public ContactData(String firstname, String lastname, String address, String home, String email, String group) {
-    this.id = null;
+    this.id = 0;
     this.firstname = firstname;
     this.lastname = lastname;
     this.address = address;
@@ -21,7 +21,7 @@ public class ContactData {
     this.group = group;
   }
 
-  public ContactData(String id, String firstname, String lastname, String address, String home, String email, String group) {
+  public ContactData(int id, String firstname, String lastname, String address, String home, String email, String group) {
     this.id = id;
     this.firstname = firstname;
     this.lastname = lastname;
@@ -31,8 +31,12 @@ public class ContactData {
     this.group = group;
   }
 
-  public String getId() {
+  public int getId() {
     return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 
   public String getFirstname() {
@@ -59,6 +63,7 @@ public class ContactData {
     return group;
   }
 
+
   @Override
   public String toString() {
     return "ContactData{" +
@@ -73,7 +78,7 @@ public class ContactData {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     ContactData that = (ContactData) o;
-    return Objects.equals(id, that.id) &&
+    return id == that.id &&
             Objects.equals(firstname, that.firstname) &&
             Objects.equals(lastname, that.lastname);
   }
--- addressbook-web-tests/src/test/java/pl/stqa/pdt/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pdt/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.stqa.pdt.addressbook.model.ContactData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -11,10 +12,21 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testAddNewContact() {
     List<ContactData> before = app.getContactHelper().getContactList();
-    app.getContactHelper().createContact(new ContactData("Asia", "Duda", "Kraków",
-            "111333222", "a@b.com", "test1"),true);
+    ContactData contact = new ContactData("Asia", "Duda", "Kraków",
+            "111333222", "a@b.com", "test1");
+    app.getContactHelper().createContact(contact,true);
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+    int max = 0;
+    for(ContactData c : after){
+      if (c.getId() > max){
+        max = c.getId();
+      }
+    }
+    contact.setId(max);
+    before.add(contact);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 
