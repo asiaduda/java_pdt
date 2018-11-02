2844d0cb9cd22e7570efb9f8f0e023e550c2a53d
Asia Duda <asiaduda@gmail.com>
2018-11-02 12:11:24 +0100

    Caching the results of long-lasting operations for groups

--- addressbook-web-tests/src/test/java/pl/stqa/pdt/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pdt/addressbook/appmanager/GroupHelper.java
@@ -55,6 +55,7 @@ public class GroupHelper extends HelperBase {
     initGroupCreation();
     fillGroupForm(group);
     submitGroupCreation();
+    groupChache = null;
     returnToGroupPage();
   }
 
@@ -63,12 +64,14 @@ public class GroupHelper extends HelperBase {
     initGroupModification();
     fillGroupForm(group);
     submitGroupModification();
+    groupChache = null;
     returnToGroupPage();
   }
 
   public void delete(GroupData group) {
     selectGroupById(group.getId());
     deleteSelectedGroups();
+    groupChache = null;
     returnToGroupPage();
   }
 
@@ -80,14 +83,19 @@ public class GroupHelper extends HelperBase {
     return wd.findElements(By.name("selected[]")).size();
   }
 
+  private Groups groupChache = null;
+
   public Groups all() {
-    Groups groups = new Groups();
+    if (groupChache != null){
+      return new Groups(groupChache);
+    }
+    groupChache = new Groups();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements){
       String name = element.getText();
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-      groups.add(new GroupData().withId(id).withName(name));
+      groupChache.add(new GroupData().withId(id).withName(name));
     }
-    return groups;
+    return new Groups(groupChache);
   }
 }
